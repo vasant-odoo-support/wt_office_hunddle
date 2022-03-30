43b070b01d92ba5442885d2a1704853dbc8dce2a
Vasant Chauhan <v92281@gmail.com>
2022-03-30 21:11:11 +0530

    Job page fix and link

--- controllers/main.py
+++ controllers/main.py
@@ -1393,10 +1393,58 @@ class WebsiteHuddleCustom(http.Controller):
         print("==========")
         return  request.render('wt_office_hunddle.our_team_tepmplate')
 
-    @http.route('/jobs-new', type='http', auth='public', website=True)
-    def job_office_huddle(self):
+    @http.route('/jobs-office-huddle', type='http', auth='public', website=True)
+    def job_office_huddle(self, country=None, department=None, office_id=None, **kwargs):
         print("==========")
-        return  request.render('wt_office_hunddle.job_tepmplate')
+        env = request.env(context=dict(request.env.context, show_address=True, no_tag_br=True))
+
+        Country = env['res.country']
+        Jobs = env['hr.job']
+
+        # List jobs available to current UID
+        domain = request.website.website_domain()
+        job_ids = Jobs.search(domain, order="is_published desc, no_of_recruitment desc").ids
+        # Browse jobs as superuser, because address is restricted
+        jobs = Jobs.sudo().browse(job_ids)
+
+        # Default search by user country
+        if not (country or department or office_id or kwargs.get('all_countries')):
+            country_code = request.session['geoip'].get('country_code')
+            if country_code:
+                countries_ = Country.search([('code', '=', country_code)])
+                country = countries_[0] if countries_ else None
+                if not any(j for j in jobs if j.address_id and j.address_id.country_id == country):
+                    country = False
+
+        # Filter job / office for country
+        if country and not kwargs.get('all_countries'):
+            jobs = [j for j in jobs if j.address_id is None or j.address_id.country_id and j.address_id.country_id.id == country.id]
+            offices = set(j.address_id for j in jobs if j.address_id is None or j.address_id.country_id and j.address_id.country_id.id == country.id)
+        else:
+            offices = set(j.address_id for j in jobs if j.address_id)
+
+        # Deduce departments and countries offices of those jobs
+        departments = set(j.department_id for j in jobs if j.department_id)
+        countries = set(o.country_id for o in offices if o.country_id)
+
+        if department:
+            jobs = [j for j in jobs if j.department_id and j.department_id.id == department.id]
+        if office_id and office_id in [x.id for x in offices]:
+            jobs = [j for j in jobs if j.address_id and j.address_id.id == office_id]
+        else:
+            office_id = False
+
+        # Render page
+        vals = {
+            'jobs': jobs,
+            'countries': countries,
+            'departments': departments,
+            'offices': offices,
+            'country_id': country,
+            'department_id': department,
+            'office_id': office_id,
+        }
+        return  request.render('wt_office_hunddle.job_tepmplate', vals)
 
     @http.route('/web-development', type='http', auth='public', website=True)
     def website_development_office_huddle(self):
